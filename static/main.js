a410b82d60019943e0cbe39874b5f64b7a88459f
Mrsheikhhh <sheikhahmed45670@gmail.com>
2023-05-06 08:21:12 +0500

    m

--- static/main.js
+++ static/main.js
@@ -117,7 +117,7 @@ const cancel = () => {
 
 const copy = async() => {
     const roomid = document.getElementById("roomid").innerText;
-    await navigator.clipboard.writeText("http://localhost:3030/join/" + roomid);
+    await navigator.clipboard.writeText("https://ahmed-video.vercel.app//join/" + roomid);
 };
 const invitebox = () => {
     $("#getCodeModal").modal("show");
